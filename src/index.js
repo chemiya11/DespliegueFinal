d8ebfbfe9d95462ab2331b5cbd82412d8671c956
José María Lozano Olmedo <116385651+chemiya@users.noreply.github.com>
2023-06-21 13:30:55 +0200

    prueba

---
+++ src/index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+
+const route = require("./controlador/controlador");
+const cors = require('cors')
+const port = ( 3000)
+
+const app = express();
+const PORT = process.env.PORT ;
+
+global.__basedir = __dirname;
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("frontendRailway"));
+app.use('/api', route)
+
+
+
+app.listen(PORT,(error)=>{
+  if(error)
+  {console.log('error al iniciar el servidor: '+error)}
+  else{
+      console.log('servidor iniciado en el prueto: '+PORT)
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
